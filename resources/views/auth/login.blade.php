1ea8f83296877640dc84a84ed918c82d953e1bb2
wbcastilho <welber.castilho@yahoo.com.br>
2022-03-17 20:25:59 -0300

    Alteracao titulo pagina login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,7 +8,7 @@
   <!-- CSRF Token -->
   <meta name="csrf-token" content="{{ csrf_token() }}">
 
-  <title>Engenharia</title>
+  <title>Novatele</title>
 
   <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
   <link rel="stylesheet" href="{{asset('css/app.css')}}">
